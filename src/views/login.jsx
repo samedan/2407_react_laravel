6c4268a5a64057834bc8e817990cfe80750c2073
Daniel Popescu <same.dan@gmail.com>
2024-07-23 17:13:20 +0200

    Register Frontend done

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,24 +1,23 @@
 import { Link } from "react-router-dom";
 
-export default function login() {
-    const Submit = (ev) => {
-        ev.preventDefault();
-    };
+export default function Login() {
+  const Submit = (ev) => {
+    ev.preventDefault();
+  };
 
-    return (
-        <div className="login-signup-form animated fadeinDown">
-            <div className="form">
-                <h1 className="title">Login to your account</h1>
-                <form onSubmit={Submit}>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Pasword" />
-                    <button className="btn btn-block">Login</button>
-                    <p className="message">
-                        Not registered?{" "}
-                        <Link to="/register">Create a new account</Link>
-                    </p>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="login-signup-form animated fadeinDown">
+      <div className="form">
+        <h1 className="title">Login to your account</h1>
+        <form onSubmit={Submit}>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Pasword" />
+          <button className="btn btn-block">Login</button>
+          <p className="message">
+            Not registered? <Link to="/register">Create a new account</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/views/register.jsx
+++ src/views/register.jsx
@@ -1,23 +1,50 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "../../axiosClient";
+import { useStateContext } from "./../contexts/useStateContext";
 
-export default function register() {
-    const Submit = (ev) => {
-        ev.preventDefault();
+export default function Register() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const { setUser, setToken } = useStateContext();
+
+  const Submit = (ev) => {
+    ev.preventDefault();
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
     };
-    return (
-        <div className="login-signup-form animated fadeinDown">
-            <div className="form">
-                <h1 className="title">Create a new account</h1>
-                <form onSubmit={Submit}>
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Pasword" />
-                    <button className="btn btn-block">Register</button>
-                    <p className="message">
-                        Already registered? <Link to="/login">Login </Link>
-                    </p>
-                </form>
-            </div>
-        </div>
-    );
+    console.log(payload);
+    axiosClient
+      .postForm("register", payload)
+      .then(({ data }) => {
+        setUser(data.user);
+        setToken(data.token);
+      })
+      .catch((err) => {
+        const response = err.response;
+        if (response && response.status === 422) {
+          console.log(response.data.errors);
+        }
+      });
+  };
+  return (
+    <div className="login-signup-form animated fadeinDown">
+      <div className="form">
+        <h1 className="title">Create a new account</h1>
+        <form onSubmit={Submit}>
+          <input ref={nameRef} type="text" placeholder="Name" />
+          <input ref={emailRef} type="email" placeholder="Email" />
+          <input ref={passwordRef} type="password" placeholder="Pasword" />
+          <button className="btn btn-block">Register</button>
+          <p className="message">
+            Already registered? <Link to="/login">Login </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
